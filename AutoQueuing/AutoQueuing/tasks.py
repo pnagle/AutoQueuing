f06cb48d23a92873950deac0e805b184c29a4aa7
Piyush Nagle <piyushiit.cse@gmail.com>
2017-12-26 18:28:22 +0530

    debuggin tasks celery

--- AutoQueuing/AutoQueuing/tasks.py
+++ AutoQueuing/AutoQueuing/tasks.py
@@ -1,13 +1,19 @@
 from AutoQueuing.celeryapp import app as celery
 from DriverApp.models import Request
 from django.utils import timezone
+from celery.contrib import rdb
 
 
 @celery.task
 def update_ongoing_request(pk):
+    rdb.set_trace()
+
     if pk is not None:
         try:
+            rdb.set_trace()
             d = Request.objects.filter(request_id=pk).update(status=2, completed_at=timezone.now())
+            rdb.set_trace()
+            print "pkpkkp" + pk
             if d >= 1:
                 print 'My request_id ' + pk + ' is complete'
         except Exception as e:
